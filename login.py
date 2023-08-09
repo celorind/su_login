412ee036a8ee6cb1cee36b34db3da906ca757842
Kenta Asahi <asaken1023@gmail.com>
2023-08-09 16:28:39 +0900

    ok

--- login.py
+++ login.py
@@ -2,19 +2,41 @@
 
 import requests
 from sys import exit
+from getpass import getpass
+
+SUCESS_TEXT = "Login Success"
 
 def main():
-    id = input("ID? ")
-    passwd = input("Password? ")
+    payload = {'uid': input("ID: "), 'pwd': getpass("Password: ")}
 
-    payload = {'uid': id, 'pwd': passwd}
-    r = requests.post("https://login.shinshu-u.ac.jp/cgi-bin/Login.cgi", params=payload)
-    print(r.url)
-    if r.status_code == requests.codes.ok:
-        print("Login Sucess!")
-    else:
-        print("Login Failed!!")
+    try:
+        print("Attempting Login to ACSU Network...")
+        r = requests.post("https://login.shinshu-u.ac.jp/cgi-bin/Login.cgi", data=payload, timeout=3)
+        # with open("response.log", "wb") as f:
+        #     f.write(r.content)
+        if SUCESS_TEXT in r.text:
+            print("Login Success!")
+            exit()
+        else:
+            print("Login Failed!")
+            print("The ID/password you enterd seems wrong.")
+            exit(1)
+    
+    except requests.exceptions.ConnectionError as e:
+        print("Login Failed!")
+        print("HTTP connection error.")
+        exit(2)
+
+    except requests.exceptions.Timeout as e:
+        print("Login Failed!")
+        print("HTTP request timed out.")
+        exit(3)
+
+    except Exception as e:
+        print("Login Failed!")
+        print(e)
         exit(-1)
+
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -4,20 +4,36 @@ import requests
 from sys import exit
 from getpass import getpass
 
+SUCESS_TEXT = "Login Success"
+
 def main():
-    # input params
-    # id = input("ID: ")
-    # passwd = getpass("Password: ")
     payload = {'uid': input("ID: "), 'pwd': getpass("Password: ")}
 
     try:
-        print("Attempting Login ACSU Network...")
+        print("Attempting Login to ACSU Network...")
         r = requests.post("https://login.shinshu-u.ac.jp/cgi-bin/Login.cgi", data=payload, timeout=3)
-        with open("response.log", "wb") as f:
-            f.write(r.content)
+        # with open("response.log", "wb") as f:
+        #     f.write(r.content)
+        if SUCESS_TEXT in r.text:
+            print("Login Success!")
+            exit()
+        else:
+            print("Login Failed!")
+            print("The ID/password you enterd seems wrong.")
+            exit(1)
+    
+    except requests.exceptions.ConnectionError as e:
+        print("Login Failed!")
+        print("HTTP connection error.")
+        exit(2)
+
     except requests.exceptions.Timeout as e:
-        print("Timed out:", e)
+        print("Login Failed!")
+        print("HTTP request timed out.")
+        exit(3)
+
     except Exception as e:
+        print("Login Failed!")
         print(e)
         exit(-1)
 
